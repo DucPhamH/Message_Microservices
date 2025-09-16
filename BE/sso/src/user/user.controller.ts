47f665a5217d07f909f07a4c0c18480ce992eff9
AnhNguyen208 <nguyenletuananh20082001@gmail.com>
2025-09-16 22:45:24 +0700

    update api get user infor v1.0

--- BE/sso/src/user/user.controller.ts
+++ BE/sso/src/user/user.controller.ts
@@ -1,27 +1,21 @@
-import { Controller, Get, Body, Patch, Param, Headers } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Request } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Public } from 'src/global/decorator/public.decorator';
 import { ZodSerializerDto } from 'nestjs-zod';
 import { GetUserResponseDto } from './dto/get-user.dto';
-import { JwtTokenService } from 'src/global/jwt/jwt-token.service';
-
 
 @Controller('user')
 export class UserController {
   constructor(
     private userService: UserService,
-    private jwtService: JwtTokenService,
   ) { }
 
-  @Public()
   @Get()
   @ZodSerializerDto(GetUserResponseDto)
-  async findOne(@Headers() header: any): Promise<GetUserResponseDto> {
-    const [type, token] = header?.authorization?.split(' ') ?? [];
-    const payload = await this.jwtService.validateAccessToken(token);
-
-    return await this.userService.findOne(payload.userId);
+  async findOne(@Request() req): Promise<GetUserResponseDto> {
+    const userId = req.user.userId;
+    return await this.userService.findOne(userId);
   }
 
   @Public()
